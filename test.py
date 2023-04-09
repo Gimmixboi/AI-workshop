b4a1d07a9d2fffa9e1ef8f9fb91a22fb30817a13
Gimmixboi <127659666+Gimmixboi@users.noreply.github.com>
2023-04-09 14:44:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -40,21 +40,21 @@ with tab2:
   evaluations = pickle.load(open('evals.all.sav','rb'))
   st.dataframe(evaluations)
   x = evaluations.columns
-    fig = px.Figure(data=[
-        px.Bar(name = 'Decision Tree',
-               x = x,
-               y = evaluations.loc['Decision Tress']),
-        px.Bar(name = 'Random Forest',
-               x = x,
-               y =  evaluations.loc['Random Forest']),
-        px.Bar(name = 'KNN',
-               x = x,
-               y =  evaluations.loc['KNN']),
-        px.Bar(name = 'AdaBoost',
-               x = x,
-               y =  evaluations.loc['AdaBoost']),
-        px.Bar(name = 'XGBoost',
-               x = x,
-               y =  evaluations.loc['XGBoost'])
-    ])
+  fig = px.Figure(data=[
+      px.Bar(name = 'Decision Tree',
+             x = x,
+             y = evaluations.loc['Decision Tress']),
+      px.Bar(name = 'Random Forest',
+             x = x,
+             y =  evaluations.loc['Random Forest']),
+      px.Bar(name = 'KNN',
+             x = x,
+             y =  evaluations.loc['KNN']),
+      px.Bar(name = 'AdaBoost',
+             x = x,
+             y =  evaluations.loc['AdaBoost']),
+      px.Bar(name = 'XGBoost',
+             x = x,
+             y =  evaluations.loc['XGBoost'])
+  ])
   st.plotly_chart(fig, use_container_width=True)
